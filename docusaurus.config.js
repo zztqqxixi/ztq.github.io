40daa9bd98f64289e654ed9cde3b8482dc751e96
zztq <1725204251@qq.com>
2025-07-14 22:15:42 +0800

    Initial commit with GitHub Pages setup

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -28,7 +28,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'zztqqxixi', // Usually your GitHub org/user name.
-  projectName: 'ztq.github.io', // Usually your repo name.
+  projectName: 'ZGTQ', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
